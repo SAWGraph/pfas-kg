614e3387da6f8b9920a7d4ed51b8b244051fc73d
kschweikert <kschweik@gmail.com>
2025-02-18 15:00:28 -0500

    update ghg prefix

--- datasets/federal/us-ghg/releases-ghg.py
+++ datasets/federal/us-ghg/releases-ghg.py
@@ -34,12 +34,12 @@ metadata_dir = None
 output_dir = root_folder / "federal/us-ghg/"
 
 ##namespaces
-us_epa_ghg = Namespace(f'http://sawgraph.spatialai.org/v1/us-epa-ghg#')
-us_epa_ghg_data = Namespace(f'http://sawgraph.spatialai.org/v1/us-epa-ghg-data#')
+us_epa_ghg = Namespace(f'http://sawgraph.spatialai.org/v1/us-ghg#')
+us_epa_ghg_data = Namespace(f'http://sawgraph.spatialai.org/v1/us-ghg-data#')
 us_frs = Namespace(f"http://sawgraph.spatialai.org/v1/us-frs#")
 us_frs_data = Namespace(f"http://sawgraph.spatialai.org/v1/us-frs-data#")
-qudt = Namespace(f'https://qudt.org/schema/qudt/')
-unit = Namespace(f'https://qudt.org/vocab/unit')
+qudt = Namespace(f'http://qudt.org/schema/qudt/')
+unit = Namespace(f'http://qudt.org/vocab/unit/')
 coso = Namespace(f'http://sawgraph.spatialai.org/v1/contaminoso#')
 geo = Namespace(f'http://www.opengis.net/ont/geosparql#')
 
@@ -55,10 +55,12 @@ logging.info(f"Running triplification for ghg {state}")
 
 def main():
     df = load_data()
-    kg = triplify(df)
+    kg, facility_kg = triplify(df)
 
-    kg_turtle_file = "us-epa-ghg-releases-"+state.strip()+".ttl".format(output_dir)
+    kg_turtle_file = "us-ghg-releases-"+state.strip()+".ttl".format(output_dir)
     kg.serialize(kg_turtle_file, format='turtle')
+    facility_turtle_file = "us-ghg-facilities-"+state.strip()+".ttl".format(output_dir)
+    facility_kg.serialize(facility_turtle_file, format='turtle')
     logger = logging.getLogger(f'Finished triplifying ghg releases for {state}.')
 
 
@@ -114,15 +116,24 @@ def get_attributes(row):
 def get_iris(release):
     extra_iris = {}
 
-    chemicalName = str(release['Chemical']).replace(' ', '').replace('(', "_").replace(')', '_').replace(',',
-                                                                                                          '_').replace(';', '_').replace('[', '_').replace(']', '_').replace('/', '_')
-    facilityId = 'GHGFacility-' + str(release['GHG_id']) + '.Year-' + str(release['Year']) + '.Chemical-' + chemicalName
+    chemicalName = str(release['Chemical']).replace(' ', '').replace('(', "").replace(')', '').replace(',','').replace(';', '').replace('[', '').replace(']', '').replace('/', '')
+    
+    #if len(chemicalName)<15:
+    #    chemId = chemicalName
+    #elif 'CAS' in release.keys():
+    #    chemId = str(release['CAS']).replace(" ","").replace(",", "_").strip()
+    #else:
+    #    chemId = chemicalName
+    recordId = 'GHGFacility-' + str(release['GHG_id']) + '.' + str(release['Year']) + '.' + chemicalName
+    subpart = release['GHG_subpart'][0:release['GHG_subpart'].find(':')] #take only letter part of subpart name as iri
 
-    extra_iris['ReleaseObservation'] = us_epa_ghg[
-        'd.ReleaseObservation.' + facilityId]
-    extra_iris['Chemical'] = us_epa_ghg['d.Chemical.' + chemicalName]
-    extra_iris['Measurement'] = us_epa_ghg['d.ContaminantMeasurement.' + facilityId + '.Chemical-' + chemicalName]
-    extra_iris['Amount'] = us_epa_ghg['d.Amount.' + facilityId + '.Chemical-' + chemicalName]
+
+    extra_iris['ReleaseObservation'] = us_epa_ghg_data[
+        'd.ReleaseObservation.' + recordId]
+    extra_iris['Chemical'] = us_epa_ghg_data['d.Chemical.' + chemicalName]
+    extra_iris['Measurement'] = us_epa_ghg_data['d.ContaminantMeasurement.' + recordId]
+    extra_iris['Amount'] = us_epa_ghg_data['d.Amount.' + recordId]
+    extra_iris['Subpart'] = us_epa_ghg_data['d.Subpart.' + subpart]
 
     if 'FRS_ID' not in release.keys():
         #hard coding the 6 facilities that don't have FRS_ID from ezquery match
@@ -160,8 +171,8 @@ def triplify(df):
         kg.add((extra_iris['ReleaseObservation'], RDF.type, us_epa_ghg['GHG-ReleaseObservation']))
         kg.add((extra_iris['ReleaseObservation'], coso['hasTemporalCoverage'],
                 extra_iris['TimeInterval']))  #TODO this needs beginning and end attributes
-        kg.add((extra_iris['ReleaseObservation'], us_epa_ghg['regulatedBy'], Literal(release['GHG_subpart'],
-                                                                                     datatype=XSD.string)))  #TODO this should be done as a controlled vocabulary
+        kg.add((extra_iris['ReleaseObservation'], us_epa_ghg['ghgSubpart'], extra_iris['Subpart']))
+        kg.add((extra_iris['Subpart'], RDFS.label, Literal(release['GHG_subpart'], datatype=XSD.string)))  
 
         #facility
         if 'FRS_Facility' in extra_iris.keys():
@@ -177,17 +188,23 @@ def triplify(df):
 
         #substance
         kg.add((extra_iris['ReleaseObservation'], coso['ofSubstance'], extra_iris['Chemical']))
-        kg.add((extra_iris['Chemical'], RDF.type, us_epa_ghg['GHG-Chemical']))
+        kg.add((extra_iris['Chemical'], us_epa_ghg['chemicalName'], Literal(release['Chemical'], datatype=XSD.string)))
+        #kg.add((extra_iris['Chemical'], RDF.type, us_epa_ghg['GHG-Chemical']))
         if 'ChemicalFormula' in release.keys():
-            kg.add((extra_iris['Chemical'], us_epa_ghg['chemicalFormula'],
-                    Literal(release['ChemicalFormula'], datatype=XSD.string)))
+            kg.add((extra_iris['Chemical'], us_epa_ghg['chemicalFormula'], Literal(release['ChemicalFormula'], datatype=XSD.string))) #TODO this data has a mess of comma separated, comma in formula and alternates separated by semicolon. This will require some regex to clean up
         if 'CAS' in release.keys():
-            kg.add((extra_iris['Chemical'], us_epa_ghg['casNumber'], Literal(release['CAS'], datatype=XSD.string)))
+            kg.add((extra_iris['Chemical'], coso['casNumber'], Literal(release['CAS'], datatype=XSD.string)))
+            if release['CAS'].find(',') > -1:
+                kg.add((extra_iris['Chemical'], RDF.type, us_epa_ghg['GHG-Chemical-Collection']))
+            else: kg.add((extra_iris['Chemical'], RDF.type, us_epa_ghg['GHG-Chemical']))
+        else:
+            kg.add((extra_iris['Chemical'], RDF.type, us_epa_ghg['GHG-Chemical']))
 
         #measurement
         kg.add((extra_iris['ReleaseObservation'], coso['hasResult'], extra_iris['Measurement']))
-        kg.add((extra_iris['Measurement'], qudt['quantityKind'], extra_iris['Amount'])) #this needs to change to quantityValue and quantityKind needs to be instantiated separately
-        kg.add((extra_iris['Measurement'], RDF.type, us_epa_ghg['Measurement']))
+        kg.add((extra_iris['Measurement'], qudt['quantityKind'], coso['VolumeQuantityKind']))
+        kg.add((extra_iris['Measurement'], qudt['quantityValue'], extra_iris['Amount'])) 
+        kg.add((extra_iris['Measurement'], RDF.type, us_epa_ghg['GHG-Measurement']))
 
         #amount (quantity value)
         kg.add((extra_iris['Amount'], RDF.type, qudt['QuantityValue']))
@@ -196,7 +213,7 @@ def triplify(df):
 
         #TODO quantity kind / coso:ContaminationProperty
 
-    return kg
+    return kg, facility_kg
 
 
 if __name__ == "__main__":
